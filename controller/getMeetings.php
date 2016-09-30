0c9d11cd1947e203e17b2969dd25180ec9d5ee1a
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2016-09-30 01:04:06 -0300

    Fix getMeetings to generate own checksum

--- controller/getMeetings.php
+++ controller/getMeetings.php
@@ -1,17 +1,23 @@
 <?php
 
 $data_to_post = array();
-$data_to_post['checksum'] = CHECKSUM;
+//$data_to_post['checksum'] = CHECKSUM;
+
+        $params          = "getMeetings".SALT;
+
+        $checksum        = sha1($params);
+
+	$params		 = "getMeetings";
+	$params		.= "?checksum={$checksum}";
 
 $curl = curl_init();
 curl_setopt_array($curl, Array(
-        CURLOPT_URL            => BIGBLUEBUTTON_API."getMeetings",
+        CURLOPT_URL            => BIGBLUEBUTTON_API."$params",
         CURLOPT_TIMEOUT        => 120,
         CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_RETURNTRANSFER => TRUE,
         CURLOPT_ENCODING       => 'UTF-8',
         CURLOPT_POST           => sizeof($data_to_post),
-        CURLOPT_POSTFIELDS     => $data_to_post
 ));
 
 $data = curl_exec($curl);
