373a6fc60bd951d720ddaeda11e0a340673de543
Diogo Munhoz Fraga <digmunhoz@gmail.com>
2016-10-28 09:38:45 -0200

    Fix merge problems

--- controller/menu.php
+++ controller/menu.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'home.php'){echo 'active'; }else { echo ''; } ?>"><a href='home.php'>Home <span class='sr-only'>(current)</span></a></li>
-<li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'list_rooms.php'){echo 'active'; }else { echo ''; } ?>"><a href='list_rooms.php'>Listar Salas <span class='sr-only'>(current)</span></a></li>
-<li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'create_rooms.php'){echo 'active'; }else { echo ''; } ?>"><a href='create_rooms.php'>Criar Sala de Aula</a></li> 
-=======
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
                 <li class="<?php if(basename($_SERVER['SCRIPT_NAME']) == 'dashboard.php'){echo 'active'; }else { echo ''; } ?>">
@@ -24,4 +19,3 @@
                 </li>
             </ul>
         </div>
->>>>>>> 757ead1e9621a6ae79399c33b008fc2789da516f
